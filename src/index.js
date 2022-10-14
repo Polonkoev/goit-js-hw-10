be34d357a80aa1eeb215995f3eb439fb683555b5
polonkoev <polonkoev.temerlan@gmail.com>
2022-10-14 17:52:10 +0600

    fixed query

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
-import {fetchCountries} from "./fetchCountries"
-const inputEl = document.getElementById('search-box')
-const list = document.querySelector('.country-list')
-const divEl = document.querySelector('.country-info')
-
-
+import { fetchCountries } from './fetchCountries';
+const inputEl = document.getElementById('search-box');
+const list = document.querySelector('.country-list');
+const divEl = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
 
-let dataFromServer = []
-
-
-function inputHandler(name){
-  fetchCountries(name)
+function inputHandler(name) {
+  fetchCountries(name);
   fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,language`
   )
@@ -23,25 +18,37 @@ function inputHandler(name){
       }
       return response.json();
     })
-    .then(data => {
-    data.map(element => {
-      dataFromServer.push(element)
-      });
-    })
+    .then(data => {renderMarkupUnique(data)})
     .catch(error => {
       console.log(error);
     });
-    
-    
 }
 
-inputEl.addEventListener('input', debounce((event) => {
-  inputHandler(event.target.value)
+
+function renderMarkupUnique(data){
+  data.map((item) => list.insertAdjacentHTML('afterbegin', `<li style="
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+"><img width="60px" height="40px" src="${item.flags.svg}">&nbsp<p>${item.name.official}<p/></li>`))
+}
 
 
+
+
+
+
+
+
+
+
+
+
+inputEl.addEventListener(
+  'input',
+  debounce(event => {
+    inputHandler(event.target.value);
   }, DEBOUNCE_DELAY)
+);
 
-  
-)
-
-console.log(dataFromServer);
+// console.log(dataFromServer);
